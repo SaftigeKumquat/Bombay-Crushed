6f623136ae83947d883ada270c6cc4cab6e4f751
Matthias Bach <marix@marix.org>
2012-05-03 21:25:56 +0200

    Refactoring
    Moved state to an own file

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 
 var http = require('http');
-var Cookies = require('cookies');
 var ejs = require('./ejs.js');
 var lf = require('./lfcli.js');
 var querystring = require('querystring');
-var url = require('url');
 var fs = require('fs');
 
 var config = require('./config.js');
@@ -923,59 +921,7 @@ mapU2F = function(state, url_mappings, pattern_mappings) {
 	}
 }
 
-/**
- * Create an object that keeps the state during a full request / response
- * cycle. This state will not be kept between request. It's purpose is to
- * bundle all data collected while building the response to one request.
- */
-var createState = function(req, res) {
-	var state = {
-		'request': req,
-		'result': res,
-		'fail': function(logmessage, errorcode) {
-			serverError(state, logmessage, errorcode);
-		},
-		'cookies': new Cookies(req, res),
-		'sendToLogin': function(message) {
-			// TODO pass message to template
-			ejs.render(state, '/login.tpl');
-		},
-		'context': {
-				'meta': {'currentpage': ''}
-		},
-		'url': url.parse(req.url, true)
-	};
-
-	var session_key, user_id;
-
-	// convenience..
-	state.session_key = function(key) {
-		if(key === undefined) {
-			if(session_key === undefined) {
-				session_key = state.cookies.get('session_key');
-			}
-			return session_key;
-		} else {
-			session_key = key;
-			state.cookies.set('session_key', session_key);
-			return session_key;
-		}
-	}
-	state.user_id = function(id) {
-		if(id === undefined) {
-			if(user_id === undefined) {
-				user_id = state.cookies.get('user_id');
-			}
-			return user_id;
-		} else {
-			user_id = id;
-			state.cookies.set('user_id', user_id);
-			return user_id;
-		}
-	}
-
-	return state;
-}
+var State = require('./state.js')(serverError);
 
 server = function() {
 	// make sure uncaught exceptions don't kill the server
@@ -994,7 +940,7 @@ server = function() {
 	});
 
 	var server = http.createServer(function (req, res) {
-		var state = createState(req, res);
+		var state = State.create(req, res);
 		mapU2F(state, url_mapping, pattern_mapping);
 	});
 	if(config.listen.host) {
---
+++ state.js
@@ -0,0 +1,60 @@
+var Cookies = require('cookies');
+var url = require('url');
+
+module.exports = function(serverError) {
+	return {
+		/**
+		 * Create an object that keeps the state during a full request / response
+		 * cycle. This state will not be kept between request. It's purpose is to
+		 * bundle all data collected while building the response to one request.
+		 */
+		'create': function(req, res) {
+			var state = {
+				'request': req,
+				'result': res,
+				'fail': function(logmessage, errorcode) {
+					serverError(state, logmessage, errorcode);
+				},
+				'cookies': new Cookies(req, res),
+				'sendToLogin': function(message) {
+					// TODO pass message to template
+					ejs.render(state, '/login.tpl');
+				},
+				'context': {
+						'meta': {'currentpage': ''}
+				},
+				'url': url.parse(req.url, true)
+			};
+
+			var session_key, user_id;
+
+			// convenience..
+			state.session_key = function(key) {
+				if(key === undefined) {
+					if(session_key === undefined) {
+						session_key = state.cookies.get('session_key');
+					}
+					return session_key;
+				} else {
+					session_key = key;
+					state.cookies.set('session_key', session_key);
+					return session_key;
+				}
+			}
+			state.user_id = function(id) {
+				if(id === undefined) {
+					if(user_id === undefined) {
+						user_id = state.cookies.get('user_id');
+					}
+					return user_id;
+				} else {
+					user_id = id;
+					state.cookies.set('user_id', user_id);
+					return user_id;
+				}
+			}
+
+			return state;
+		}
+	}
+}
