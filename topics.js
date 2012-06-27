577b641a430b328e84a7c27c5c59ca8403c4b9c7
Matthias Bach <marix@marix.org>
2012-06-27 20:54:49 +0200

    Show only users units on topics pages
    Closes #19

--- topics.js
+++ topics.js
@@ -1,104 +1,108 @@
 var lf = require('./lfcli.js');
 
 var topics = function(state, render) {
-	var byArea = [];
-	var byUnit = [];
-	var units = [];
-	var areaId, unitId;
-	var areasFound = 0; 
-	var areasResolved = 0;
-	var unitsFound = 0; 
-	var unitsResolved = 0;
-	var issuesFinished = false;
 
-	var builtArea;	
-	var builtUnit;
-	var builtUnits = [];
+	var tmp, tmp2, i;
+
+	var units_by_id;
+	var unit_id;
+	var unit;
+	var areas;
+	var areas_by_id;
+	var areas_by_unitid;
+	var area;
+	var privs;
+	var issues_by_areaid;
+	var issue;
 
 	var finish = function() {
-		// check all querys have come back
-				if(issuesFinished && areasFound <= areasResolved
-			&& unitsFound <= unitsResolved ) {
-			
-			for(var i = 0; i < units.length; i++) {
-				if(units[i] !== undefined && byUnit[i] !== undefined) {
-					builtUnit = {
-						'id': units[i].id,
-						'name': units[i].name,
-						'description': units[i].description,
-						'areas': []
-					};
-					// if description is empty, at least show the name
-					if(units[i].description === "") {
-						builtUnit.description = builtUnit.name;
-					}
-					// sort areas by direct members
-					byUnit[i].sort(function(a,b){return b.direct_member_count - a.direct_member_count;});
+		var i_area, i_issue;
+		var builtUnits = [];
+		var builtUnit;
+		var buildArea;
+		var issues;
 
-					for(var a = 0; a < byUnit[i].length; a++) {
-						currentArea = byUnit[i][a];
-						
-						status1 = status2 = status3 = status4 = status5 = status6 = 0;	
-						if(byArea[currentArea.id] !== undefined) {					
-							for(var b = 0; b < byArea[currentArea.id].length; b++) {
-								currentIssue = byArea[currentArea.id][b];
-								switch(currentIssue.state) {
-									case "admission":
-										status1++;
-										break;
-									case "discussion":
-										status2++;
-										break;
-									case "verification":
-										status3++;
-										break;
-									case "voting":
-										status4++;
-										break;
-									case "canceled_revoked_before_accepted":
-										status6++;
-										break;
-									case "canceled_issue_not_accepted":
-										status6++;
-										break;
-									case "canceled_after_revocation_during_discussion":
-										status6++;
-										break;
-									case "canceled_after_revocation_during_verification":
-										status6++;
-										break;
-									case "calculation":
-										status5++;
-										break;
-									case "canceled_no_initiative_admitted":
-										status6++;
-										break;
-									case "finished_without_winner":
-										status5++;
-										break;
-									case "finished_with_winner":
-										status5++;
-										break;
-								}
+		// check all querys have come back
+		if(units_by_id !== undefined && areas_by_id !== undefined
+		   && areas_by_unitid !== undefined && issues_by_areaid !== undefined) {
+
+			for(unit_id in units_by_id) {
+				unit = units_by_id[unit_id];
+				builtUnit = {
+					'id': unit.id,
+					'name': unit.name,
+					'description': unit.description,
+					'areas': []
+				};
+				// if description is empty, at least show the name
+				if(unit.description === "") {
+					builtUnit.description = builtUnit.name;
+				}
+				// sort areas by direct members
+				areas_by_unitid[unit.id].sort(function(a,b){return b.direct_member_count - a.direct_member_count;});
+				for(i_area = 0; i_area < areas_by_unitid[unit.id].length; i_area++) {
+					area = areas_by_unitid[unit.id][i_area];
+
+					status1 = status2 = status3 = status4 = status5 = status6 = 0;
+					issues = issues_by_areaid[area.id];
+					if(issues !== undefined) {
+						for(i_issue = 0; i_issue < issues.length; i_issue++) {
+							issue = issues[i_issue];
+							switch(issue.state) {
+								case "admission":
+									status1++;
+									break;
+								case "discussion":
+									status2++;
+									break;
+								case "verification":
+									status3++;
+									break;
+								case "voting":
+									status4++;
+									break;
+								case "canceled_revoked_before_accepted":
+									status6++;
+									break;
+								case "canceled_issue_not_accepted":
+									status6++;
+									break;
+								case "canceled_after_revocation_during_discussion":
+									status6++;
+									break;
+								case "canceled_after_revocation_during_verification":
+									status6++;
+									break;
+								case "calculation":
+									status5++;
+									break;
+								case "canceled_no_initiative_admitted":
+									status6++;
+									break;
+								case "finished_without_winner":
+									status5++;
+									break;
+								case "finished_with_winner":
+									status5++;
+									break;
 							}
 						}
-
-						builtArea = {
-							'name': currentArea.name,
-							'topics': {
-								'status1': status1,
-								'status2': status2,
-								'status3': status3,
-								'status4': status4,
-								'status5': status5,
-								'status6': status6
-							}
-						};
-						builtUnit.areas.push(builtArea);
-						
 					}
-					builtUnits.push(builtUnit);
+
+					builtArea = {
+						'name': area.name,
+						'topics': {
+							'status1': status1,
+							'status2': status2,
+							'status3': status3,
+							'status4': status4,
+							'status5': status5,
+							'status6': status6
+						}
+					};
+					builtUnit.areas.push(builtArea);
 				}
+				builtUnits.push(builtUnit);
 			}
 
 			state.context.units = builtUnits;
@@ -109,43 +113,50 @@ var topics = function(state, render) {
 	// get all issues
 	lf.query('/issue', {}, state, function(res) {
 		// store issues by area
-		for(var i = 0; i < res.result.length; i++) {
-			areaId = res.result[i].area_id;
-			if(byArea[areaId] === undefined) {
-				byArea[areaId] = [];
-				areasFound++;
+		tmp = {}
+		for(i = 0; i < res.result.length; i++) {
+			issue = res.result[i];
+			if(tmp[issue.area_id] === undefined) {
+				tmp[issue.area_id] = [];
 			}
-			byArea[areaId].push(res.result[i]);
+			tmp[issue.area_id].push(issue);
 		}
-		issuesFinished = true;
+		issues_by_areaid = tmp;
 		finish();
 	});
 
-	// get all areas
-	lf.query('/area', {}, state, function(area_res) {
-		// store areas by unit
-		for(var i = 0; i < area_res.result.length; i++) {
-			areasResolved++;
-			unitId = area_res.result[i].unit_id;
-			if(byUnit[unitId] === undefined) {
-				byUnit[unitId] = [];
-				unitsFound++;
-			}
-			byUnit[unitId].push(area_res.result[i]);
-		}
-		finish();
-	});
+	// query users units
+	lf.query('/privilege', {'member_id': state.user_id(), 'include_units': true}, state,  function(res) {
+		privs = res.result;
+		units_by_id = res.units;
 
-	// get all units
-	lf.query('/unit', {}, state, function(unit_res) {
-		// store units
-		for(var i = 0; i < unit_res.result.length; i++) {
-			unitsResolved++;
-			units[unit_res.result[i].id] = unit_res.result[i];
+		var unit_ids = '';
+		for(var unit_id in units_by_id) {
+			if(unit_ids !== '') {
+				unit_ids += ',';
+			}
+			unit_ids += unit_id;
 		}
-		finish();
+
+		// query of the users units areas
+		lf.query('/area', {'unit_id': unit_ids}, state, function(res) {
+			areas = res.result;
+			tmp = {};
+			tmp2 = {}
+			for(i = 0; i < areas.length; i++) {
+				area = areas[i];
+				tmp[area.area_id] = area;
+				if(tmp2[area.unit_id] === undefined) {
+					tmp2[area.unit_id] = [];
+				}
+				tmp2[area.unit_id].push(area);
+			}
+
+			areas_by_id = tmp;
+			areas_by_unitid = tmp2;
+			finish();
+		});
 	});
-		
 }
 
 exports.get = topics;
