b446ba1cd35fef71c43d3971f7efc88b8db3b004
Matthias Bach <marix@marix.org>
2012-08-18 12:35:02 +0200

    Show implemented/not implemented in suggestion screen.
    refs 33

--- suggestion.js
+++ suggestion.js
@@ -16,11 +16,19 @@ exports.show = function(state) {
 
 	var suggestion_id = state.url.query.suggestion_id;
 
+	// the variables to that will be set by the data retrievers
+	// and if set the page will be rendered
+	var suggestion_info, opinions_info, i_say_implemented_info;
+
 	var finish = function() {
 		var ctx = state.context;
-		if(ctx.suggestion) {
+
+		if(suggestion_info !== undefined && opinions_info !== undefined && i_say_implemented_info !== undefined) {
+			ctx.suggestion = suggestion_info;
+			suggestion_info.opinions = opinions_info;
+			suggestion_info.isayimplemented = i_say_implemented_info;
+
 			ctx.meta.currentpage = "suggestion";
-			console.log(JSON.stringify(ctx));
 			ejs.render(state, '/suggestion.tpl');
 		}
 	}
@@ -35,7 +43,7 @@ exports.show = function(state) {
 
 		var total_supporters = initiative.supporter_count;
 
-		var suggestion = {
+		var tmp_suggestion = {
 			initiative: {
 				name: initiative.name
 			},
@@ -62,53 +70,77 @@ exports.show = function(state) {
 			implementedshouldnotsupporter: suggestion_res.minus1_fulfilled_count,
 			implementedmustnotsupporter: suggestion_res.minus2_fulfilled_count,
 		};
-		notimplementedneutralsupporter = suggestion.neutralsupporter;
-		implementedneutralsupporter = suggestion.neutralsupporter;
+		notimplementedneutralsupporter = tmp_suggestion.neutralsupporter;
+		implementedneutralsupporter = tmp_suggestion.neutralsupporter;
 
-		suggestion.mustsupportwidth = suggestion.mustsupporter * 100 / total_supporters + "%";
-		suggestion.shouldsupportwidth = suggestion.shouldsupporter * 100 / total_supporters + "%";
-		suggestion.neutralsupportwidth = suggestion.neutralsupporter * 100 / total_supporters + "%";
-		suggestion.shouldnotsupportwidth = suggestion.shouldnotsupporter * 100 / total_supporters + "%";
-		suggestion.mustnotsupportwidth = suggestion.mustnotsupporter * 100 / total_supporters + "%";
+		tmp_suggestion.mustsupportwidth = tmp_suggestion.mustsupporter * 100 / total_supporters + "%";
+		tmp_suggestion.shouldsupportwidth = tmp_suggestion.shouldsupporter * 100 / total_supporters + "%";
+		tmp_suggestion.neutralsupportwidth = tmp_suggestion.neutralsupporter * 100 / total_supporters + "%";
+		tmp_suggestion.shouldnotsupportwidth = tmp_suggestion.shouldnotsupporter * 100 / total_supporters + "%";
+		tmp_suggestion.mustnotsupportwidth = tmp_suggestion.mustnotsupporter * 100 / total_supporters + "%";
 
-		suggestion.notimplementedmustsupportwidth = suggestion.notimplementedmustsupporter * 100 / total_supporters + "%";
-		suggestion.notimplementedshouldsupportwidth = suggestion.notimplementedshouldsupporter * 100 / total_supporters + "%";
-		suggestion.notimplementedneutralsupportwidth = suggestion.notimplementedneutralsupporter * 100 / total_supporters + "%";
-		suggestion.notimplementedshouldnotsupportwidth = suggestion.notimplementedshouldnotsupporter * 100 / total_supporters + "%";
-		suggestion.notimplementedmustnotsupportwidth = suggestion.notimplementedmustnotsupporter * 100 / total_supporters + "%";
-		suggestion.implementedmustsupportwidth = suggestion.notimplementedmustsupporter * 100 / total_supporters + "%";
-		suggestion.implementedshouldsupportwidth = suggestion.notimplementedshouldsupporter * 100 / total_supporters + "%";
-		suggestion.implementedneutralsupportwidth = suggestion.notimplementedneutralsupporter * 100 / total_supporters + "%";
-		suggestion.implementedshouldnotsupportwidth = suggestion.notimplementedshouldnotsupporter * 100 / total_supporters + "%";
-		suggestion.implementedmustnotsupportwidth = suggestion.notimplementedmustnotsupporter * 100 / total_supporters + "%";
-//	 "isayimplemented": true, TODO
-
-		suggestion.opinions = []; // TODO
-		// format:
-		//, "opinions": [
-		//			{ "user": { "nick": "joknopp", "name": "Johannes Knopp", "picsmall": "content_img/profile_delegate_3.png", "picmini": "content_img/profile_small.png" }, "action": "for",
-		//				"implemented": true, "smiley": 1 },
-		//			{ "user": { "nick": "incredibul", "name": "Christophe Chan Hin", "picsmall": "content_img/profile_delegate_2.png", "picmini": "content_img/profile_small.png" }, "action": "against",
-		//				"implemented": false, "smiley": 2 },
-		//			{ "user": { "nick": "cfritzsche", "name": "Christoph Fritzsche", "picsmall": "content_img/profile_delegate_1.png", "picmini": "content_img/profile_small.png" }, "action": "for",
-		//				"implemented": true, "smiley": 3 },
-		//			{ "user": { "nick": "themarix", "name": "Matthias Bach", "picsmall": "content_img/profile_delegate_4.png", "picmini": "content_img/profile_small.png" }, "action": "against",
-		//				"implemented": false, "smiley": 4 }
-		//		], "opinionpage": 1, "opinionpages": 2
+		tmp_suggestion.notimplementedmustsupportwidth = tmp_suggestion.notimplementedmustsupporter * 100 / total_supporters + "%";
+		tmp_suggestion.notimplementedshouldsupportwidth = tmp_suggestion.notimplementedshouldsupporter * 100 / total_supporters + "%";
+		tmp_suggestion.notimplementedneutralsupportwidth = tmp_suggestion.notimplementedneutralsupporter * 100 / total_supporters + "%";
+		tmp_suggestion.notimplementedshouldnotsupportwidth = tmp_suggestion.notimplementedshouldnotsupporter * 100 / total_supporters + "%";
+		tmp_suggestion.notimplementedmustnotsupportwidth = tmp_suggestion.notimplementedmustnotsupporter * 100 / total_supporters + "%";
+		tmp_suggestion.implementedmustsupportwidth = tmp_suggestion.notimplementedmustsupporter * 100 / total_supporters + "%";
+		tmp_suggestion.implementedshouldsupportwidth = tmp_suggestion.notimplementedshouldsupporter * 100 / total_supporters + "%";
+		tmp_suggestion.implementedneutralsupportwidth = tmp_suggestion.notimplementedneutralsupporter * 100 / total_supporters + "%";
+		tmp_suggestion.implementedshouldnotsupportwidth = tmp_suggestion.notimplementedshouldnotsupporter * 100 / total_supporters + "%";
+		tmp_suggestion.implementedmustnotsupportwidth = tmp_suggestion.notimplementedmustnotsupporter * 100 / total_supporters + "%";
 
 		// add author info
 		lf.query('/member', { 'member_id': suggestion_res.author_id }, state, function(res) {
 			var author = res.result[0];
 			console.log("AUTHOR: " + JSON.stringify(author));
 
-			suggestion.author = {
+			tmp_suggestion.author = {
 				nick: author.name,
 				name: author.realname || author.name,
 				picsmall: "avatar/" + author.id,
 				picmini: "picbig/" + author.id
 			}
 
-			state.context.suggestion = suggestion;
+			suggestion_info = tmp_suggestion;
+			finish();
+		});
+	});
+
+	lf.query('/opinion', { 'suggestion_id': suggestion_id }, state, function(res) {
+		console.log('OPINIONS:' + JSON.stringify(res));
+		var tmp_i_say_implemented = false;
+		var members_to_resolve = '';
+
+		// TODO handle opinion-pages
+
+		for(var i = 0; i < res.result.length; i++) {
+			var lf_opinion = res.result[i];
+			if(lf_opinion.member_id == state.user_id()) {
+				tmp_i_say_implemented = lf_opinion.fulfilled;
+			}
+
+			if(members_to_resolve != '') {
+				members_to_resolve += ',';
+			}
+			members_to_resolve += lf_opinion.member_id;
+		}
+		i_say_implemented_info = tmp_i_say_implemented;
+
+		lf.query('/member', { 'member_id': members_to_resolve }, state, function(res) {
+			console.log('MEMBERS:' + JSON.stringify(res));
+			opinions_info = []; // TODO
+			// format:
+			//, "opinions": [
+			//			{ "user": { "nick": "joknopp", "name": "Johannes Knopp", "picsmall": "content_img/profile_delegate_3.png", "picmini": "content_img/profile_small.png" }, "action": "for",
+			//				"implemented": true, "smiley": 1 },
+			//			{ "user": { "nick": "incredibul", "name": "Christophe Chan Hin", "picsmall": "content_img/profile_delegate_2.png", "picmini": "content_img/profile_small.png" }, "action": "against",
+			//				"implemented": false, "smiley": 2 },
+			//			{ "user": { "nick": "cfritzsche", "name": "Christoph Fritzsche", "picsmall": "content_img/profile_delegate_1.png", "picmini": "content_img/profile_small.png" }, "action": "for",
+			//				"implemented": true, "smiley": 3 },
+			//			{ "user": { "nick": "themarix", "name": "Matthias Bach", "picsmall": "content_img/profile_delegate_4.png", "picmini": "content_img/profile_small.png" }, "action": "against",
+			//				"implemented": false, "smiley": 4 }
+			//		], "opinionpage": 1, "opinionpages": 2
 			finish();
 		});
 	});
