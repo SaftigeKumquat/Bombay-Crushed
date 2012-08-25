4c83301145366e159d5c51da7fb1f33e6f0c6483
Christoph Fritzsche <christoph.fritzsche@gmail.com>
2012-08-25 16:28:05 +0200

    removed recent requirement in delegations, fixed some bugs fixes #48

--- delegations.js
+++ delegations.js
@@ -2,6 +2,24 @@
  * Utility functions for querying delegations
  */
 var lf = require('./lfcli.js');
+var userFunc = require('./user.js');
+
+// Basic functions on JS objects
+var registerFunctions = function() {
+/**
+ * checks if an object is contained in an array
+ *
+ * @param obj object to be checked
+ */
+	Array.prototype.contains = function(obj) {
+    		for (var i = 0; i < this.length; i++) {
+        		if (this[i] === obj) {
+            			return true;
+        		}
+    		}
+    		return false;
+	}
+}
 
 /**
  * Query all data required for the delegations box on the overview page
@@ -14,6 +32,8 @@ var lf = require('./lfcli.js');
  */
 exports.lastActions = function(state, render) {
 	var delegations;
+
+	registerFunctions();
 
 	/**
 	 * Wrapper for the external callback that stores the results into
@@ -35,38 +55,46 @@ exports.lastActions = function(state, render) {
 		var i, resolved = 0;
 		var resolved_delegations = [];
 		var pending_resolves = 0;
+		var trustees = [];
 
 		// follow the delegations and resolve the user information
 		if(links.length) {
 			for(i = 0; i < links.length; i++) {
-				pending_resolves++; // don't finish until we know who it is and what he did
-				console.log('Query trustee name: ' + links[i].trustee_id);
+				
+				// check we don't query a trustee twice
+				if(trustees.contains(links[i].trustee_id)) {
+					continue;
+				}
+				else {
+					trustees.push(links[i].trustee_id);
+					pending_resolves++; // don't finish until we know who it is and what he did
+					console.log('Query trustee name: ' + links[i].trustee_id);
+				}
+
 				lf.query('/member', {'member_id': links[i].trustee_id }, state, function(res) {
 					var delegate = res.result[0];
 					console.log(JSON.stringify(delegate));
 
 					var info_obj = {
-						user: {
-							'name': delegate.name,
-							'picsmall': 'avatar/' + delegate.id
-						}
+						'user': userFunc.getUserBasic(delegate)
 					};
 					// get last action of user
 					lf.query('/vote', {'member_id': delegate.id,
-					                   'issue_state': 'finished_with_winner,finished_without_winner',
-					                   'issue_closed_after': delegate.last_activity
+					                   'issue_state': 'finished_with_winner,finished_without_winner'
 					                  }, state, function(res) {
-						if(res.res && res.res.length > 0) {
+						if(res.result && res.result.length > 0) {
+							console.log(JSON.stringify(res));
 							// TODO sort these properly instead of taking an arbitrary one
-							vote = res.res[0];
+							vote = res.result[0];
 							if(vote.grade.grade > 0) {
 								info_obj.action = 'for';
 							} else {
 								info_obj.action = 'against';
 							}
 							// get information about the initiative the last action was performed on
-							lfapi.query('/initiative', {'initiative_id': vote.initiative_id}, state, function(res) {
-								info_obj.title = res.res[0].name;
+							lf.query('/initiative', {'initiative_id': vote.initiative_id}, state, function(res) {
+								info_obj.title = res.result[0].name;
+								info_obj.initiative_id = res.result[0].id;
 								resolved_delegations.push(info_obj);
 								pending_resolves--; // we now know what she did
 								// if all delegations have been handled finish it up
